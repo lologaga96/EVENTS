9a14cd4e353e465c2b9a649d5d9a97232fc62763
Carmel GBOHOUI <carmelgbohoui@gmail.com>
2022-08-13 22:00:02 +0200

    mon premier commit

---
+++ database/migrations/2022_08_13_195046_create_evenements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->timestamp("date_demarrage");
+            $table->date("date_fin");
+            $table->string("description");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evenements');
+    }
+};
