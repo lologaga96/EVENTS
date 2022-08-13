eb80a235570e857f9e8f5537f5b97360e48839e5
Carmel GBOHOUI <carmelgbohoui@gmail.com>
2022-08-13 22:18:17 +0200

    creation de la vue

---
+++ resources/views/formulaire.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ajouter Un EVENEMENT</title>
+</head>
+<body>
+    <h1>mon formulaire</h1>
+</body>
+</html>
